163f5d9b63821995439545b9d41aaeb7f17632c3
wang <wyb_cn@126.com>
2025-09-28 08:14:05 +0800

    Add files via upload

---
+++ Countdown/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Countdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dTP = new System.Windows.Forms.DateTimePicker();
+            this.CB_wave_OK = new System.Windows.Forms.ComboBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 112);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "重新开始计时";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "剩余";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dTP
+            // 
+            this.dTP.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dTP.Location = new System.Drawing.Point(43, 39);
+            this.dTP.Name = "dTP";
+            this.dTP.ShowUpDown = true;
+            this.dTP.Size = new System.Drawing.Size(90, 26);
+            this.dTP.TabIndex = 3;
+            this.dTP.Value = new System.DateTime(2025, 9, 24, 0, 25, 0, 0);
+            // 
+            // CB_wave_OK
+            // 
+            this.CB_wave_OK.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_wave_OK.FormattingEnabled = true;
+            this.CB_wave_OK.Location = new System.Drawing.Point(45, 71);
+            this.CB_wave_OK.Name = "CB_wave_OK";
+            this.CB_wave_OK.Size = new System.Drawing.Size(88, 24);
+            this.CB_wave_OK.TabIndex = 4;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "☟";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 156);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.CB_wave_OK);
+            this.Controls.Add(this.dTP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "倒计时";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dTP;
+        private System.Windows.Forms.ComboBox CB_wave_OK;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Countdown/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Countdown
+{
+
+    
+    public partial class Form1 : Form
+    {
+        static public string mpath = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+        static public string maininiFile = mpath + "Countdown.ini";
+        int cur_s = 0;
+        int max_s = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cgdtp = dTP.Value;
+            int h = cgdtp.Hour;
+            int m = cgdtp.Minute;
+            int s = cgdtp.Second;
+            max_s = h * 60 * 60 + m * 60 + s;
+            cur_s = 0;
+            timer1.Enabled = true;
+            OperIni.INIWrite(maininiFile, "main", "SelectedIndex", CB_wave_OK.SelectedIndex);
+            OperIni.INIWrite(maininiFile, "main", "h", dTP.Value.Hour);
+            OperIni.INIWrite(maininiFile, "main", "m", dTP.Value.Minute);
+
+            OperIni.INIWrite(maininiFile, "main", "s", dTP.Value.Second);
+
+
+            OperIni.INIWrite(maininiFile, "main", "Left", Left);
+            OperIni.INIWrite(maininiFile, "main", "Top", Top);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cur_s++;
+            int lost=(max_s-cur_s);
+            string showtxt = "";
+            if (lost / 60 / 60 == 0)
+            {
+                showtxt = string.Format("{0:00}", (lost / 60) % 60).ToString() + ":" + string.Format("{0:00}", (lost % 60 % 60)).ToString();
+            }
+            else
+            {
+                showtxt = (lost / 60 / 60).ToString() + ":" + string.Format("{0:00}", (lost / 60) % 60).ToString() + ":" + string.Format("{0:00}", (lost % 60 )).ToString();
+            }
+
+             
+            label1.Text = "还有" + showtxt;
+            Text = "" + showtxt;
+
+           // notifyIcon1.Text = showtxt;
+            notifyIcon1.ShowBalloonTip(2000, showtxt, showtxt, ToolTipIcon.Info );
+            
+
+            if (cur_s >= max_s) 
+            {
+                //timer1.Stop();
+                timer1.Enabled = false;
+                label1.Text = "END";
+                Text = "倒计时";
+               // notifyIcon1.Text = "倒计时";
+                if(CB_wave_OK.SelectedIndex>-1)
+                    playsound(mpath+"/"+CB_wave_OK.Text);
+            }
+        }
+        [DllImport("winmm.dll", EntryPoint = "sndPlaySoundA", CallingConvention = CallingConvention.Cdecl)]
+        private static extern long sndPlaySoundA(string lpszSoundName, long uFlags);
+        public  void playsound(String filename)
+        {
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                    player.SoundLocation = filename;// "音乐文件名";
+                    player.Load();
+                    player.Play();
+                    //sndPlaySoundA(filename, (long)0x0001);
+                }
+            }
+            catch (Exception ex)
+            {
+               // Form_main.log.Error("sndPlaySound 错误:" + ex.Message.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MaximizeBox = false;
+            DirectoryInfo di = new DirectoryInfo(mpath);
+            foreach (FileInfo fi in di.GetFiles("*.wav"))
+            {
+                CB_wave_OK.Items.Add(fi.Name);
+            }
+            if (CB_wave_OK.Items.Count > 0)
+            {
+                CB_wave_OK.SelectedIndex = OperIni.INIRead(maininiFile, "main", "SelectedIndex", 0);
+            }
+
+           // imgpath = OperIni.INIRead(maininiFile, "main", "SelectedIndex", 0);
+           // file_fname = OperIni.INIRead(maininiFile, "main", "file_fname", "img");
+           //dTP.Value.Hour
+            //dTP.Value.Year = 2026;
+           // int h = OperIni.INIRead(maininiFile, "main", "h", 0);
+
+            DateTime dt3 = new DateTime(2015, 12, 31, OperIni.INIRead(maininiFile, "main", "h", 0), OperIni.INIRead(maininiFile, "main", "m", 25), OperIni.INIRead(maininiFile, "main", "s", 0));
+            dTP.Value = dt3;
+            //dTP.Value.AddHours(OperIni.INIRead(maininiFile, "main", "h", 0));
+            //dTP.Value.AddMinutes(OperIni.INIRead(maininiFile, "main", "m", 0));
+
+            Left = OperIni.INIRead(maininiFile, "main", "Left", Left);
+            Top = OperIni.INIRead(maininiFile, "main", "Top", Top);
+
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            OperIni.INIWrite(maininiFile, "main", "Left", Left);
+            OperIni.INIWrite(maininiFile, "main", "Top", Top);
+
+           //窗口销毁时，获取控件属性可能不正确。
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();                                //窗体显示
+            this.WindowState = FormWindowState.Normal;  //窗体状态默认大小
+            this.Activate(); 
+        }
+    }
+}
